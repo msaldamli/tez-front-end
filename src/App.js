93d2e763dab422afb13ad02fc2305b4e5144ead6
Muhammed said saldamlı <msaldamli48@gmail.com>
2023-05-14 23:56:12 +0300

    header eklendi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
 
 function App() {
-  return <div className='App'>TEZ İÇİN FRONT END</div>;
+  return (
+    <div className='App'>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
